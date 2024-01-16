72c56b335a695302a319f3a223f7747696e7e0bd
Carl Allen Torno <ctorno30@gmail.com>
2024-01-17 04:11:22 +0800

    some changes

--- sandbox/test.py
+++ sandbox/test.py
@@ -1,5 +1,7 @@
-x = 23
-y = 10
 
-print(id(x), id(y))
-print(x is y)
+print("Printing current and previous number sum in a range(10)")
+
+prev = 0
+for x in range (0,10):
+    print(f"Current Number {x} Previous Number is {prev} Sum = {x + prev}")
+    prev = x
